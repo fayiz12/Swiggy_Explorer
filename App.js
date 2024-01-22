ae8d7fe8be9cdf3230d09b2ea874082605e5228c
muhammed fayiz <muhammedfayiz@muhammeds-MacBook-Air-2.local>
2024-01-22 12:52:45 +0530

    done some exersice with component and element

--- App.js
+++ App.js
@@ -1,37 +1,39 @@
-const heading=React.createElement(
-    "div",
-    {id:'parent'},
-    [
-        React.createElement(
-            "div",
-            {id:'child'},
-            [React.createElement(
-                "h1",
-                {},
-                "hey there iam a h1 tag")
-            ,React.createElement(
-                "h2",
-                {},
-                "hey iam h2")
-            ]),
-        React.createElement(
-            "div",
-            {id:'child'},
-            [React.createElement(
-                "h1",
-                {},
-                "hey there iam a h1 tag")
-            ,React.createElement(
-                "h2",
-                {},
-                "hey iam h2")
-            ])
-    ]
-    )
+import React from "react"
+import ReactDOM  from "react-dom"
 
 
+// this is the core react element
+const content=React.createElement("h1",{id:"content"},"this is the content part")
 
+
+// this is the react element using jsx
+const heading1=<h1>hey there</h1>
+
+const Heading=()=><h1>hey there its me functional component</h1>
+
+//this is functional component in react
+const Footer=()=>{
+        return(
+            <div>
+                {/* this are the 3 ways to write functional component inside a functional component(component composition)*/}
+                <Heading></Heading>
+                <Heading/>
+                {Heading()}
+                 {/* this is how we can use react element inside a functional component*/}
+                {content}
+                <h2>hey i am footer</h2>
+            </div>
+        )
+}
+
+const element=(<div>
+    <h1>i am element</h1>
+    {/* this is how a functional component is written inside a react element */}
+    <Footer/>
+</div>)
 
 
 const root=ReactDOM.createRoot(document.getElementById('root'))
-root.render(heading)
+
+// this is how the functional component is rendered
+root.render(Footer())
